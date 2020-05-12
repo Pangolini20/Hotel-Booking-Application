53098076720239bc0080a80bb7e56fb5fe697164
Darius <opritoiudarius@yahoo.com>
2020-05-12 10:06:02 +0300

    Initial Commit Implemented the Login UI and a small project structure

---
+++ src/controllers/Login.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Login
+{
+    public void window()
+    {
+
+
+    }
+
+    public void doSth()
+    {
+        System.out.println("do");
+
+    }
+}
