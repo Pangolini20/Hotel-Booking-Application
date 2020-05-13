448808d8675ca463ced1d98276e2f49f645084d9
Darius <opritoiudarius@yahoo.com>
2020-05-14 01:23:58 +0300

    Hashed the passwords https://darius-paul.atlassian.net/browse/HOT-27

--- src/Database/Customer.java
+++ src/Database/Customer.java
@@ -1,68 +1,21 @@
 package Database;
 
-public class Customer implements java.io.Serializable{
+public class Customer extends User implements java.io.Serializable{
 
-    private String username,password,full_name,phone,role;
+    public Customer() {
+    }
 
     public Customer(String username, String password, String full_name, String phone) {
-        this.username = username;
-        this.password = password;
-        this.full_name = full_name;
-        this.phone = phone;
-    }
-
-    public Customer()
-    {
-
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFull_name() {
-        return full_name;
-    }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+        super(username, password, full_name, phone);
     }
 
     @Override
     public String toString() {
-        return "Client{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", full_name='" + full_name + '\'' +
-                ", phone='" + phone + '\'' +
+        return "Customer{" +
+                "username='" + getUsername() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", full_name='" + getFull_name() + '\'' +
+                ", phone='" + getPhone() + '\'' +
                 '}';
     }
 }
