86538b3cfd4dcfc210846c5504e19c0d707f448b
Darius <opritoiudarius@yahoo.com>
2020-05-17 18:42:19 +0300

    Database can load now

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
+
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/controllers/Login.java
+++ src/controllers/Login.java
@@ -14,26 +14,44 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-import java.util.List;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import static Database.Security.generateHash;
 
 public class Login implements Initializable
 {
+
     @FXML Button log_button;
     @FXML TextField user_text;
     @FXML PasswordField pass_field;
     @FXML Button reg_button;
 
-    private List<User> list;
+    private ArrayList<User> database;
 
-    private void check_credentials()
-    {
+    private boolean check_credentials() throws NoSuchAlgorithmException {
+        for(User e : database)
+        {
+            String input_password=generateHash(pass_field.getText());
 
+            if(e.getUsername().equals(user_text.getText()))
+                if(e.getPassword().equals(input_password))
+                    return true;
+                else
+                    return false;
+        }
+
+        return false;
     }
 
     public void authentification()
     {
-
+        try {
+            System.out.println(check_credentials());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
      public void goto_register()
@@ -53,11 +71,17 @@ public class Login implements Initializable
         stage.setScene(scene);
         stage.show();
 
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        LoadDatabase.readDB();
+       LoadDatabase obj=new LoadDatabase();
+        obj.readDB();
+        database=obj.getArray();
+
+       /* for(User e : database)
+        {
+            System.out.println(e.toString());
+        }*/
     }
 }
